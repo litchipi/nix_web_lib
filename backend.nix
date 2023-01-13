be83202c1b53811183d3078b930c39b0cbb1a0c4
Litchi Pi <litchi.pi@proton.me>
2023-01-13 11:48:06 +0100

    backend: rust: Pass arguments directly to makeePackageSet
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- backend.nix
+++ backend.nix
@@ -2,22 +2,20 @@
   rust = {
     build = {
       src,
-      rustChannel,
-      rustVersion,
-      cargo2nix_file,
+      cargo2nix_file ? "${src}/Cargo.nix",
       bin_name ? "backend",
-      ... }: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            cargo2nix.overlays.default
-          ];
-        };
-        targets = pkgs.rustBuilder.makePackageSet {
-          inherit rustChannel rustVersion;
-          packageFun = import cargo2nix_file;
-        };
-      in
-        (targets.workspace."${bin_name}" {}).bin;
+      rustBuilderArgs ? {},
+    }: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          cargo2nix.overlays.default
+        ];
+      };
+      targets = pkgs.rustBuilder.makePackageSet ({
+        packageFun = import cargo2nix_file;
+      } // rustBuilderArgs);
+    in
+      (targets.workspace."${bin_name}" {}).bin;
   };
 }
