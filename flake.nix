77a54c7c3cce240e4c201cf613d975ed5e740e08
Litchi Pi <litchi.pi@proton.me>
2023-02-12 13:41:15 +0100

    flake: override rust-overlay input of cargo2nix with one following nixpkgs
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- flake.nix
+++ flake.nix
@@ -2,16 +2,22 @@
   description = "Builds Web components with nix";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/22.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Rust
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     cargo2nix = {
       url = github:cargo2nix/cargo2nix;
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
     };
   };
 
